1e91d1b4328898973c19c1838b5f4b3d3ce08b84
Shion <sano400jp@gmail.com>
2017-09-19 10:55:09 +0000

    配布ページのトップに配布済み件数を表示

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,11 @@ class PagesController < ApplicationController
   end
 
   def deal
+    # チケット枚数の取得
+    @dealed_cnt = Ticket.where(lottery_code: session[:lottery_code]).size
+    if @dealed_cnt.nil?
+      @dealed_cnt = 0
+    end
+    # 
   end
 end
