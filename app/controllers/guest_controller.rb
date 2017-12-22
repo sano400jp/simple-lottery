b477ee6a19eb1736e84b084d99b1d8c9d95269df
Shion <sano400jp@gmail.com>
2017-12-22 08:06:20 +0000

    guestページ作成開始

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,7 +1,29 @@
 class GuestController < ApplicationController
   def open_lottery
+    guest_code = params[:guest_code]
+    token = params[:token]
+    # キャッシュにguest_code・tokenを登録
+    # session[:ticket_token] = token
+    # session[:guest_code] = guest_code
+
+    # 抽選済みの場合
+      # チケットのseqを取得
+    @ticket = Ticket.find_by(guest_code: guest_code, token: token)
+      # 抽選済みフラグを立てて伝搬
+    # 未抽選の場合
+      # 抽選済みフラグを折って伝搬
+
   end
 
   def result
+    # paramsからguest_codeを取得
+    # キャッシュからトークンを取得
+    # lotteery_code 取得
+    # gift_seq取得
+    # 当たってたら
+      # giftsレコードを取得
+      # あたりフラグを立てて画面側に伝搬
+    # 外れてたら
+      # あたりフラグを折って画面側に伝搬
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'guest/open_lottery'
 
   get 'guest/result'
 
   root 'pages#top'
 
-  get '/lotteries', to:'lotteries#show'
-  get '/lotteries/deal', to:'pages#deal'
-  get '/lotteries/draw', to:'lotteries#draw'
-  get '/q_deal_tickets', to:'tickets#quick_deal'
-  post '/deal_tickets', to:'tickets#deal'
+  get  '/lotteries'      , to: 'lotteries#show'
+  get  '/lotteries/deal' , to: 'pages#deal'
+  get  '/lotteries/draw' , to: 'lotteries#draw'
+  get  '/q_deal_tickets' , to: 'tickets#quick_deal'
+  post '/deal_tickets'   , to: 'tickets#deal'
+
+  # ゲスト画面
+  get '/guest/:guest_code/:token' , to: 'guest#open_lottery'
+  get '/guest/result'             , to: 'guest#result'
 
   resources :owners
   resources :lotteries, param: :code
